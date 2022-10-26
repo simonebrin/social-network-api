1b0e5f726d5155c06e22f20362db11a412797955
Simone Brin <simonebrin@gmail.com>
2022-10-26 11:11:49 -0400

    started on controllers

--- controllers/user-controllers.js
+++ controllers/user-controllers.js
@@ -1,8 +1,26 @@
+const { User, Thought } = require('../models');
+
 const userController = {
-    test(req, res) {
-        console.log('route is working')
-        res.json('awesome job')
-    }
+    getAllUsers(req, res) {
+        User.find({})
+            .select('-_v')
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => {
+                console.log(err);
+                res.sendStatus(400);
+            });
+    },
+
+    
 }
+
+
+
+// const userController = {
+//     test(req, res) {
+//         console.log('route is working')
+//         res.json('awesome job')
+//     }
+// }
 
 module.exports = userController;
